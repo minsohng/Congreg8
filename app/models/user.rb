a1a8acafa998b786ebf2829feef48b39399ae5a0
Min Sang <minsohng@gmail.com>
2016-06-18 15:38:46 -0700

    user signup ver1

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ActiveRecord::Base
+  attr_accessor :password
+  validates :username, :presence => true, :uniqueness => true, :length => { :in => 3..20 }
+  validates :password, :confirmation => true #password_confirmation attr
+  validates_length_of :password, :in => 6..20, :on => :create
+end
