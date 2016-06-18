d0242544dac35895686756420a49d03c0c69d03b
Min Sang <minsohng@gmail.com>
2016-06-18 16:11:59 -0700

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160613225631) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "encrypted_password"
