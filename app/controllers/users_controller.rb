944319559b1e2ea13aea40edbcd491548c3a561e
Min Sang <minsohng@gmail.com>
2016-06-20 01:19:28 -0700

    admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index]
+
   def show
       @user = User.find(params[:id])
+  end
+
+  def index
+    @user = User.all
   end
 
   def new
@@ -22,5 +28,13 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:username, :password,
                                    :password_confirmation)
+    end
+
+    #confirms logged in user
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(username:  "Admin",
+             password:              "admin123",
+             password_confirmation: "admin123",
+             admin: true)
