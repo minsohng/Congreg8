d80f1c2f111aebe0cadc3187dceeef03f7e0a902
Min Sang <minsohng@gmail.com>
2016-07-06 11:36:33 -0700

    sessions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create!(username:  "Admin",
+User.create!(username:  "Admin123",
              password:              "admin123",
              password_confirmation: "admin123",
              admin: true)
